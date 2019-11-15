1dbe06b698a991d6d8a7dc0e05cade607cbae53b
Mason Wyatt <mason.wyatt95@gmail.com>
2019-11-14 19:11:38 -0600

    New Master

--- Assests/JS/script.js
+++ Assests/JS/script.js
@@ -1,38 +1,35 @@
 
-    //Games Live
+
+    //Games Live ajax request and response (Rapid API - NBA API Documentation)
     var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api-nba-v1.p.rapidapi.com/games/live/",
-        "method": "GET",
-        "headers": {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api-nba-v1.p.rapidapi.com/games/live/",
+      "method": "GET",
+      "headers": {
         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
         "x-rapidapi-key": "2b1f9e6733msh22bb848a9641962p19a128jsn799dd10dfb90"
-          }
-  }
-  $.ajax(settings).done(function (response) {
+      }
+    }
 
-    console.log(response)
+    $.ajax(settings).done(function (response) {
+      console.log(response)
 
-      // $(".hometeam").html("<h1>" + response.api.games[0].hTeam.shortName + "</h1>");
-      // $(".awayteam").html("<h1>" + response.api.games[0].vTeam.shortName + "</h1>");
-      // $(".gametime").html("<h1>" + response.api.games[0].clock + "</h1>");
-      // $(".quarter").html("<h1>" + response.api.games[0].currentPeriod + "</h1>");
-      // $(".homescore").html("<h1>" + response.api.games[0].hTeam.score.points + "</h1>")
-      // $(".awayscore").html("<h1>" + response.api.games[0].vTeam.score.points + "</h1>")
-      // $("#homelogo").attr("src", response.api.games[0].hTeam.logo)
-      // $("#awaylogo").attr("src", response.api.games[0].vTeam.logo)
+      // //.map function
+      var games = response.api.games;
 
-  var games = response.api.games;
+      for (var i = 0; i < games.length; i++) {
+        $(".hometeam").append("<h1>" + "Home Team Name : " + response.api.games[i].hTeam.shortName + "</h1>");
+        $(".awayteam").append("<h1>" + "Away Team Name : " + response.api.games[i].vTeam.shortName + "</h1>");
+        $(".gametime").append("<h1>" + "Time : " + response.api.games[i].clock + "</h1>");
+        $(".quarter").append("<h1>" + "Quarter : " + response.api.games[i].currentPeriod + "</h1>");
+        $(".homescore").append("<h1>" + "Home Team Score : " + response.api.games[i].hTeam.score.points + "</h1>")
+        $(".awayscore").append("<h1>" + "Away Team Score : " + response.api.games[i].vTeam.score.points + "</h1>")
+        $("#homelogo").attr("src", response.api.games[i].hTeam.logo)
+        $("#awaylogo").attr("src", response.api.games[i].vTeam.logo)
+      }
 
-  for (var i = 0; i < games.length; i++) {
-    var gameDiv = $("<div>");
-      var hteamname = games[i].hTeam.shortName
-      var p = $("<p>").html(hteamname);
-      gameDiv.append(p);
-  }
-  });
-  // Parallax
-  $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+    });
+
+
+  
