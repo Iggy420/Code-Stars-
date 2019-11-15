40795e0adbf433d73f26c07b9eaff8160c3e43de
Andrew Rice <riceandrew8@gmail.com>
2019-11-14 19:41:42 -0600

    temporary change to master branch

--- Assests/JS/standings.js
+++ Assests/JS/standings.js
@@ -9,3 +9,22 @@
         // Wins and Losses
 
         // Win Percentage
+
+
+
+        var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://api-nba-v1.p.rapidapi.com/standings/standard/2019",
+                "method": "GET",
+                "headers": {
+                        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+                        "x-rapidapi-key": "2b1f9e6733msh22bb848a9641962p19a128jsn799dd10dfb90"
+                }
+        }
+        
+        $.ajax(settings).done(function (response) {
+                console.log(response);
+        });
+
+        
